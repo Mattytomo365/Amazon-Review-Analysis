c7f8bad7bd689cfed1fab7ae080606ca7e548915
Mattytomo365 <matty.tom@icloud.com>
2025-04-14 18:55:02 +0100

    Loading data

--- Amazon_Review_Analysis.py
+++ Amazon_Review_Analysis.py
@@ -1,1 +1,20 @@
-print('Hello world')
+import pandas as pd
+import textblob
+import nltk
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def load_data(file_path):
+    """
+    Load the dataset from the CSV file.
+    """
+
+    df = pd.read_csv(file_path)
+    return df
+
+def main():
+
+    df = load_data("Reviews.csv")
+    print(df.head())
+
+main()
