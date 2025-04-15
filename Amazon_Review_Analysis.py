1d681193c22de884fc7008eb6b75e65069c84d61
Mattytomo365 <matty.tom@icloud.com>
2025-04-15 18:51:32 +0100

    Removal of unecessary columns

--- Amazon_Review_Analysis.py
+++ Amazon_Review_Analysis.py
@@ -12,9 +12,21 @@ def load_data(file_path):
     df = pd.read_csv(file_path)
     return df
 
+def preprocess_data(df):
+    """
+    Dropping unnecessary columns and renaming the columns.
+    """
+    del df['ProfileName'] # ProfileName is not useful for analysis
+    del df['UserId'] # UserId is not useful for analysis
+    del df['Summary'] # Summary provides a shortened version of the review, meaning keywords for the seniment analysis are not present
+    del df['Time'] # Time is not useful for analysis
+
+    return df
+
 def main():
 
     df = load_data("Reviews.csv")
+    df = preprocess_data(df)
     print(df)
     print(df.isnull().sum())
 
